a8fedf905533015b8b4e7dd766e7aa4803d1acb6
zhuscat <zhudx6512@163.com>
2023-11-11 23:21:30 +0800

    feat: initial commit

---
+++ src/pages/index/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>
+    <view v-if="loading">
+      loading
+    </view>
+    <slot v-else></slot>
+  </view>
+</template>
+
+<script setup>
+defineProps({
+  loading: {
+    type: Boolean,
+    default: false,
+  }
+})
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <Hello>
+      <view>slot</view>
+    </Hello>
+  </view>
+</template>
+
+<script>
+import Hello from './components/Hello.vue';
+
+export default {
+  components: {
+    Hello
+  },
+  onLoad() {},
+  methods: {},
+}
+</script>w
